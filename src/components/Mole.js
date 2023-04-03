def1813f9b3afc427e7ffe1838dc8a3bfc46d3bb
megazoor <dalmasmedia@gmail.com>
2023-04-03 00:30:04 -0400

    added three js ui and animations

--- src/components/Mole.js
+++ src/components/Mole.js
@@ -1,29 +1,88 @@
-import React from 'react';
-import dirtImage from '../assets/dirt.png';
-import moleImage from '../assets/mole.png';
-import whackedImage from '../assets/whacked.png';
+import React, { useRef, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Box } from '@react-three/drei';
 
 const Mole = ({ mole, onClick }) => {
+  const boxRef = useRef();
+  const [miniCubes, setMiniCubes] = useState([]);
+
+  useFrame(() => {
+    if (boxRef.current) {
+      boxRef.current.rotation.y += 0.01;
+    }
+  });
+
   const handleClick = () => {
     onClick(mole.id, mole.active);
+    if (mole.active) {
+      const cubes = [];
+      for (let i = 0; i < 100; i++) {
+        cubes.push({
+            id: i,
+            position: [
+              (Math.random() - 0.5) * 2,
+              (Math.random() - 0.5) * 2,
+              (Math.random() - 0.5) * 2,
+            ],
+            velocity: [
+              (Math.random() - 0.5) * 4,
+              (Math.random() - 0.5) * 4,
+              (Math.random() - 0.5) * 4,
+            ],
+          });
+        }
+        setMiniCubes(cubes);
+      }
+    };
+  
+    useFrame(() => {
+      setMiniCubes((prevCubes) =>
+        prevCubes.map((cube) => ({
+          ...cube,
+          position: [
+            cube.position[0] + cube.velocity[0] * 0.01,
+            cube.position[1] + cube.velocity[1] * 0.01,
+            cube.position[2] + cube.velocity[2] * 0.01,
+          ],
+        }))
+      );
+    });
+  
+    const moleColor = mole.active
+      ? mole.whacked
+        ? 'red'
+        : 'blue'
+      : 'gray';
+  
+    return (
+      <group onClick={handleClick}>
+        <Box
+          ref={boxRef}
+          args={[1, 1, 1]}
+          position={[
+            (mole.id % 4) * 2 - 3,
+            Math.floor(mole.id / 4) * 2 - 3,
+            0,
+          ]}
+        >
+          <meshStandardMaterial color={moleColor} />
+        </Box>
+        {miniCubes.map((cube) => (
+          <Box
+            key={cube.id}
+            args={[0.1, 0.1, 0.1]}
+            position={[
+              (mole.id % 4) * 2 - 3 + cube.position[0],
+              Math.floor(mole.id / 4) * 2 - 3 + cube.position[1],
+              0 + cube.position[2],
+            ]}
+          >
+            <meshStandardMaterial color={moleColor} />
+          </Box>
+        ))}
+      </group>
+    );
   };
-
-  const currentMoleImage = mole.whacked ? whackedImage : moleImage;
-
-  console.log(`Mole ID: ${mole.id}, Active: ${mole.active}, Whacked: ${mole.whacked}`);
-
-  return (
-    <div
-      className={`mole-container ${mole.active ? 'active' : ''}`}
-      onClick={handleClick}
-    >
-      {mole.active ? (
-        <img src={currentMoleImage} alt="mole" className="mole" />
-      ) : (
-        <img src={dirtImage} alt="dirt mound" className="dirt" />
-      )}
-    </div>
-  );
-};
-
-export default Mole;
+  
+  export default Mole;
+  
